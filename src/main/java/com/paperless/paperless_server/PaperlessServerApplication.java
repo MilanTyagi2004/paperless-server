3e4421b03ac21c6b34cba91ac2c7a8846fa1a61f
Milan Tyagi <milantyagi2004@gmail.com>
2024-10-20 20:02:15 +0530

    first commit

---
+++ src/main/java/com/paperless/paperless_server/PaperlessServerApplication.java
@@ -0,0 +1,13 @@
+package com.paperless.paperless_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperlessServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperlessServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paperless/paperless_server/PaperlessServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paperless.paperless_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperlessServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
